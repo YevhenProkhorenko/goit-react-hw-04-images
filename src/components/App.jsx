2cd54e1c168cc38952a31d03f0ab3e14b4be7ff7
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-10-05 13:33:42 +0300

    error unexpected token

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import API from './API/API';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -6,82 +6,142 @@ import Loader from './Loader/Loader';
 import Button from './Button/Button';
 import Modal from './Modal/Modal';
 
-class App extends Component {
-  state = {
-    pictures: [],
-    page: 1,
-    query: '',
-    loading: false,
-    modalOpen: false,
-    selectedImage: '',
-  };
+export default function App() {
+  const [pictures, setPictures] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
 
-  componentDidMount() {
-    this.setState({ query: '' });
-  }
+  useEffect(() => {
+    setQuery({ query: '' });
+  }, []);
+  
+  useEffect(() => {
+    fetchIMG();
+  }, [query, page]);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.query !== this.state.query) {
-      this.fetchIMG();
-    }
-  }
-
-  changeQuery = query => {
-    if (this.query !== query) {
-      this.setState({ query, page: 1 });
+  const changeQuery = query => {
+    if (query !== query) {
+      setPage(+1);
     }
   };
 
-  fetchIMG = () => {
-    const { page, query } = this.state;
-
-    this.setState({ loading: true });
+  const fetchIMG = () => {
+    setLoading(true);
     try {
-      API(query, page)
-        .then(data => {
-          this.setState(prevState => ({
-            pictures: [...prevState.pictures, ...data],
-            page: prevState.page + 1,
-          }));
-          console.log(this.state.pictures);
-        })
-        .finally(() => this.setState({ loading: false }));
-    } catch (error) {
-      console.log(error);
+      API(query, page).then(data => {
+        setPictures((prevState) => [...prevState, ...data],
+        setPage(prevState=> prevState.page +1))
+      }). finally (() => setLoading(false))
     }
+    catch(error) {console.log(error); }
+   
+    
+
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
   };
 
-  toggleModal = () => {
-    this.setState(prevState => ({
-      modalOpen: !prevState.modalOpen,
-    }));
+  const openSelectedImage = largeImageURL => {
+    setSelectedImage(largeImageURL);
+    toggleModal();
   };
 
-  openSelectedImage = largeImageURL => {
-    this.setState({ selectedImage: largeImageURL });
-    this.toggleModal();
-  };
+  const isImages = Boolean(pictures.length);
+  return (
+    <div>
+      <Searchbar onSubmit={changeQuery} />
+      {isImages && (
+        <ImageGallery items={pictures} clickOnImage={openSelectedImage} />
+      )}
 
-  render() {
-    const { pictures, loading, modalOpen, selectedImage } = this.state;
-    const { changeQuery, fetchIMG, toggleModal, openSelectedImage } = this;
-
-    const isImages = Boolean(pictures.length);
-
-    return (
-      <div>
-        <Searchbar onSubmit={changeQuery} />
-        {isImages && (
-          <ImageGallery items={pictures} clickOnImage={openSelectedImage} />
-        )}
-
-        {isImages && <Button onClick={fetchIMG} text={'Load more'} />}
-        {loading && <Loader />}
-        {modalOpen && (
-          <Modal onClose={toggleModal} largeImageURL={selectedImage} />
-        )}
-      </div>
-    );
-  }
+      {isImages && <Button onClick={fetchIMG} text={'Load more'} />}
+      {loading && <Loader />}
+      {modalOpen && (
+        <Modal onClose={toggleModal} largeImageURL={selectedImage} />
+      )}
+    </div>
+  );
 }
-export default App;
+// class App extends Component {
+//   state = {
+//     pictures: [],
+//     page: 1,
+//     query: '',
+//     loading: false,
+//     modalOpen: false,
+//     selectedImage: '',
+//   };
+
+//   componentDidMount() {
+//     this.setState({ query: '' });
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.query !== this.state.query) {
+//       this.fetchIMG();
+//     }
+//   }
+
+//   changeQuery = query => {
+//     if (this.query !== query) {
+//       this.setState({ query, page: 1 });
+//     }
+//   };
+
+//   fetchIMG = () => {
+//     const { page, query } = this.state;
+
+//     this.setState({ loading: true });
+//     try {
+//       API(query, page)
+//         .then(data => {
+//           this.setState(prevState => ({
+//             pictures: [...prevState.pictures, ...data],
+//             page: prevState.page + 1,
+//           }));
+//           console.log(this.state.pictures);
+//         })
+//         .finally(() => this.setState({ loading: false }));
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   toggleModal = () => {
+//     this.setState(prevState => ({
+//       modalOpen: !prevState.modalOpen,
+//     }));
+//   };
+
+//   openSelectedImage = largeImageURL => {
+//     this.setState({ selectedImage: largeImageURL });
+//     this.toggleModal();
+//   };
+
+//   render() {
+//     const { pictures, loading, modalOpen, selectedImage } = this.state;
+//     const { changeQuery, fetchIMG, toggleModal, openSelectedImage } = this;
+
+//     const isImages = Boolean(pictures.length);
+
+//     return (
+//       <div>
+//         <Searchbar onSubmit={changeQuery} />
+//         {isImages && (
+//           <ImageGallery items={pictures} clickOnImage={openSelectedImage} />
+//         )}
+
+//         {isImages && <Button onClick={fetchIMG} text={'Load more'} />}
+//         {loading && <Loader />}
+//         {modalOpen && (
+//           <Modal onClose={toggleModal} largeImageURL={selectedImage} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
+// export default App;
+
